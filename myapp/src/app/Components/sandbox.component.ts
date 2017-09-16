afe363e4f8003796f6b47c483bfa33fd3a930844
Brandon Smith <brandon@reddirtwebdesign.com>
2017-09-16 17:55:46 +0000

    Finished Adding types to properties

--- myapp/src/app/Components/sandbox.component.ts
+++ myapp/src/app/Components/sandbox.component.ts
@@ -11,10 +11,18 @@ export class SandboxComponent {
     name:string = 'John Doe';
     age:number = 36;
     hasChildren:boolean = true;
+    myNumbersArray:number[] = [1,2,3];
+    myStringsArray:string[] = ["Hello", "World"];
+    myTuple:[string, number] = ['hello', 35];
+    unusable:void = undefined;
+    u:undefined = undefined;
+    n:null = null;
     
     constructor(){
         this.name = 'Brandon';
         this.age = 45;
         this.hasChildren = true;
+        this.myNumbersArray = [4,5,6];
+        this.myStringsArray = ["Goodbye", "World"];
     }
 }
