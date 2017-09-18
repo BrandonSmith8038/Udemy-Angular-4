015115d689de8758f87b63d7996b17b07ef2d44b
Brandon Smith <brandon@reddirtwebdesign.com>
2017-09-18 08:44:02 +0000

    Finished local storage

--- faqapp/src/app/services/data.service.ts
+++ faqapp/src/app/services/data.service.ts
@@ -6,7 +6,8 @@ export class DataService {
   questions:Question[];
 
   constructor() { 
-    this.questions = [
+    
+    /*this.questions = [
       {
         text:'What is your name?',
         answer: 'My name is Brad',
@@ -22,15 +23,58 @@ export class DataService {
         answer: 'My favorite language is JavaScript',
         hide:true
       }
-    ];
+    ];*/
   }
 
+ 
+ //Get questions from local storage
   getQuestions(){
+    
+    if(localStorage.getItem('questions')  == null){
+      this.questions = [];
+    }else{
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+    
     return this.questions;
   }
   
+  //Add question to local storage
   addQuestion(question:Question){
     this.questions.unshift(question);
+    
+    //Init local varibale
+    
+    let questions;
+    
+    if(localStorage.getItem('questions')  == null){
+      questions = [];
+      //Push new question
+      questions.unshift(question);
+      //Set new array to local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }else{
+      
+      questions = JSON.parse(localStorage.getItem('questions'));
+      
+      //Add new question
+      questions.unshift(question);
+      
+      //Re set LS
+      
+      localStorage.setItem('questions',JSON.stringify(questions));
+      
+    }
+  }
+  
+  //Remove question to local storage
+  removeQuestion(question:Question){
+    for(let i = 0;i < this.questions.length;i++){
+        if(question == this.questions[i]){
+          this.questions.splice(i,1);
+          localStorage.setItem('questions', JSON.stringify(this.questions));
+        }
+    }
   }
 
 }
